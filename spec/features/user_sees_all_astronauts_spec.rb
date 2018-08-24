6fb5410c72778363b5eb075b8b456c5882d80a34
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-24 11:50:33 -0600

    WIP add test for average age

--- spec/features/user_sees_all_astronauts_spec.rb
+++ spec/features/user_sees_all_astronauts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'user sees all astronauts' do
+describe 'user sees all astronauts and their attributes' do
   describe 'they visit /astronauts' do
   it 'it displays all astronauts and their attributes' do
     astronaut_1 = Astronaut.create!(name: "Neil Armstrong" , age: 37, job: "Commander")
@@ -16,4 +16,18 @@ describe 'user sees all astronauts' do
     expect(page).to have_content(astronaut_2.job)
     end
   end
+
+  describe 'user sees the average age of all astronauts' do
+    describe 'they visit /astronauts' do
+      it 'displays the average age of all astronauts' do
+        astronaut_1 = Astronaut.create!(name: "Neil Armstrong" , age: 37, job: "Commander")
+        astronaut_2 = Astronaut.create!(name: "Mae Jemison" , age: 32, job: "Commander")
+
+        visit '/astronauts'
+
+        expect(page).to have_content("#{Astronaut.average_age}")
+      end
+    end
+  end
+
 end
